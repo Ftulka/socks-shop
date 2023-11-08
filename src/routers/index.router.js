516e6d9dfc58b088dbb62f02c0a171c7e2a1e3a4
kiptev001 <reactcrwk@rambler.ru>
2023-11-08 14:47:31 +0300

    bucket start

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
 const renderTemplate = require('../lib/renderTemplate');
 const Index = require('../views/Index');
+const Bucket = require('../views');
+const { Order, Position } = require('../../db/models');
 
+router.get('/', (req, res) => {
+  renderTemplate(Index, {}, res);
+});
 
-
-
-router.get('/',  (req, res) => {
-    renderTemplate(Index, {}, res);
+router.get('/bucket', async (req, res) => {
+  const order = await Order.findOne({
+    where: { userId: 1 },
+    include: [{ model: Position, raw: true }],
   });
+  console.log(order);
+  // renderTemplate(Bucket, {}, res);
+});
 
-  
-
-
-  module.exports = router;
+module.exports = router;
---
+++ src/views/Bucket.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const Layout = require('../views/Layout');
+
+const Bucket = ({ order }) => (
+  <>
+    <div>BUCKET</div>
+    <div>BUCKET</div>
+  </>
+);
+module.exports = Bucket;
