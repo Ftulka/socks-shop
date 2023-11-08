14e196e3dbc60813afa10bc39fca5171bc2516a4
kiptev001 <reactcrwk@rambler.ru>
2023-11-08 11:55:30 +0300

    измененения

--- public/js/navbar.js
+++
@@ -1,6 +0,0 @@
-console.log('navbar script connected');
-const favoritesLink = document.querySelector('#favoritesLink');
-favoritesLink.addEventListener('click', async (event) => {
-  event.preventDefault();
-  const response = await fetch();
-});
